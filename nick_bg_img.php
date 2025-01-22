27258ef8f1a277c2a9f1d0a4f5ff204de9073f08
moonhouse-80san <mbg1346@gmail.com>
2025-01-22 14:23:28 +0900

    프로파일 이미지 수정

--- nick_bg_img.php
+++ nick_bg_img.php
@@ -1,46 +1,56 @@
 <?php
-class nickBgUtil
-{
-public $nick_bg_data='{"1":"01.png","2":"02.png","3":"03.png","4":"04.png","5":"05.png","6":"06.png","7":"07.png","8":"08.png","9":"09.png","10":"10.png","11":"11.png","12":"12.png","13":"13.png","14":"14.png","15":"15.png","16":"16.png","17":"17.png","18":"18.png","19":"19.png","20":"20.png","21":"21.png","22":"22.png","23":"23.png","24":"24.png","25":"25.png","26":"26.png","27":"27.png","28":"28.png","29":"29.png","30":"30.png","31":"31.png","32":"32.png","33":"33.png","34":"34.png","35":"35.png","36":"36.png","37":"37.png","38":"38.png","39":"39.png","40":"40.png"}';
-public $nick_bg_array;
+class nickBgUtil {
+    private $nick_bg_data;
+    private $nick_bg_array;
 
-	function __construct(){
-		$this->nick_bg_array = json_decode($this->nick_bg_data,true);
-	}
+    public function __construct() {
+        // 현재 PHP 파일이 있는 디렉토리를 기준으로 상대 경로 설정
+        $profile_dir = __DIR__ . '/img/profile';
+        
+        // PNG 파일 목록 가져오기
+        $png_files = glob($profile_dir . '/*.png');
+        
+        $data = array();
+        foreach($png_files as $index => $file) {
+            // 파일명만 추출 (경로 제외)
+            $filename = basename($file);
+            // 1부터 시작하는 인덱스로 배열 생성
+            $data[$index + 1] = $filename;
+        }
+        
+        // 키 기준으로 정렬
+        ksort($data);
+        
+        // JSON 형태로 변환하여 저장
+        $this->nick_bg_data = json_encode($data);
+        $this->nick_bg_array = $data;
+    }
 
-	function overflow32($v)
-	{
-		$v = $v % 4294967296;
-		if ($v > 2147483647) return $v - 4294967296;
-		elseif ($v < -2147483648) return $v + 4294967296;
-		else return $v;
-	}
+    private function overflow32($v) {
+        $v = $v % 4294967296;
+        if ($v > 2147483647) return $v - 4294967296;
+        elseif ($v < -2147483648) return $v + 4294967296;
+        else return $v;
+    }
 
-	function hashCode( $s )
-	{
-		$h = 0;
-		$len = strlen($s);
-		for($i = 0; $i < $len; $i++)
-		{
-			$h = $this->overflow32(31 * $h + ord($s[$i]));
-		}
+    private function hashCode($s) {
+        $h = 0;
+        $len = strlen($s);
+        for($i = 0; $i < $len; $i++) {
+            $h = $this->overflow32(31 * $h + ord($s[$i]));
+        }
+        return $h;
+    }
 
-		return $h;
-	}
-
-	function getNickBgColor($s){
-		$hashStr = $this->hashCode($s);
-		$c_length = count($this->nick_bg_array);
-		$hashStr = (($hashStr % $c_length) + $c_length) % $c_length;
-		$i=0;
-		foreach($this->nick_bg_array as $cl){
-			if($i == $hashStr){
-				return $cl;
-			}
-			$i++;
-		}
-
-		return "#FF4500";
-	}
-
+    public function getNickBgColor($s) {
+        if (empty($s)) {
+            return $this->nick_bg_array[1];
+        }
+        
+        $hashStr = $this->hashCode($s);
+        $c_length = count($this->nick_bg_array);
+        $index = (($hashStr % $c_length) + $c_length) % $c_length + 1;
+        
+        return $this->nick_bg_array[$index] ?? $this->nick_bg_array[1];
+    }
 }
